b9b93ac14c4989eef8522a42a5cc728179b91dec
Nagendra <nagendrababu.marasu@gmail.com>
2024-01-27 15:07:55 +0530

    added frontend

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors())
+app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 
 let notes = [
@@ -91,7 +92,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT ||  3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
